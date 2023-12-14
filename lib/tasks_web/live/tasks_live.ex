bcedc7251652fdc6f1c4928f3afffdbeb504857b
Kristófer R <kristofer.thorlaksson@automattic.com>
2023-12-14 05:36:56 -0500

    Sanitize task details HTML before saving

--- lib/tasks_web/live/tasks_live.ex
+++ lib/tasks_web/live/tasks_live.ex
@@ -179,6 +179,7 @@ defmodule TasksWeb.TasksLive do
   end
 
   def handle_event("save", task, socket) do
+    task = Map.put(task, "details", task["details"] |> HtmlSanitizeEx.html5())
     {:noreply, assign(socket, tasks: [task | socket.assigns.tasks], form: to_form(%{}))}
   end
 
