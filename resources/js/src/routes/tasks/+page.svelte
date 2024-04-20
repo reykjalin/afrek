cc7df859a42c24b53c46ff6b8b46f3fa4b875708
Kristófer R <kristofer@thorlaksson.com>
2024-04-20 18:43:45 -0400

    add tasks page

---
+++ resources/js/src/routes/tasks/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import { goto } from '$app/navigation';
+
+	const { user } = getContext('auth');
+
+	$: {
+		if (!$user) {
+			goto('/login');
+		}
+	}
+</script>
+
+<h2>Tasks</h2>
+
+<ul>
+	<li>Task 1</li>
+	<li>Task 2</li>
+	<li>Task 3</li>
+	<li>Task 4</li>
+</ul>
