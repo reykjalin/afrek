9e4a765009cb04f8511915a4908595cfcdc6a31d
Kristófer R <kristofer@thorlaksson.com>
2024-04-20 18:41:37 -0400

    load user on page load

--- resources/js/src/routes/+layout.ts
+++ resources/js/src/routes/+layout.ts
@@ -1,1 +1,15 @@
+import { getCsrfCookie, getUser } from '$lib/api';
+
 export const ssr = false;
+
+export async function load() {
+	try {
+		await getCsrfCookie();
+		const response = await getUser();
+
+		return { user: response.data };
+	} catch (e) {
+		// Not logged in.
+		return { user: undefined };
+	}
+}
