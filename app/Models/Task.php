0088faed7ba9f3403f3cd3f4ee25557a5148c37d
Kristófer R <kristofer@thorlaksson.com>
2024-05-25 02:28:27 -0400

    add tags database migrations and model updates

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
@@ -16,4 +17,23 @@ class Task extends Model
     protected $attributes = [
     	'details' => '',
     ];
+
+    public function tags(): BelongsToMany
+	{
+		return $this->belongsToMany(Tag::class);
+	}
+
+	protected static function booted(): void
+	{
+		static::deleted(function (Task $task) {
+			foreach( $task->tags as $tag ) {
+				$tag->refresh();
+				$task->tags()->detach($tag->id);
+
+				if ( $tag->tasks->count() === 0 ) {
+					$tag->delete();
+				}
+			}
+		});
+	}
 }
