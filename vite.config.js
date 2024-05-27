855e8561903244dd63d4939002035c6e98e95dec
Kristófer R <kristofer@thorlaksson.com>
2024-05-26 23:45:13 -0400

    use esnext

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: [
-        "resources/css/style.css",
-        "resources/js/src/entrypoints/footer.ts",
-        "resources/js/src/entrypoints/header.ts",
-        "resources/js/src/entrypoints/register.ts",
-        "resources/js/src/entrypoints/login.ts",
-        "resources/js/src/entrypoints/tasks.ts",
-      ],
-      refresh: true,
-    }),
-    svelte(),
-  ],
-  build: {
-    target: "esnext",
-  },
+	plugins: [
+		laravel({
+			input: [
+				'resources/css/style.css',
+				'resources/js/src/entrypoints/footer.ts',
+				'resources/js/src/entrypoints/header.ts',
+				'resources/js/src/entrypoints/register.ts',
+				'resources/js/src/entrypoints/login.ts',
+				'resources/js/src/entrypoints/tasks.ts',
+			],
+			refresh: true,
+		}),
+		svelte(),
+	],
+	build: {
+		target: 'esnext',
+	},
 });
